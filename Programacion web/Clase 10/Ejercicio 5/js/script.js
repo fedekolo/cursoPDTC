0f831332043cb2c493b0d56b142ad564e99d9efe
Fede Kolo <fedekolonski@gmail.com>
2020-09-22 22:41:57 -0300

    ej

---
+++ Programacion web/Clase 10/Ejercicio 5/js/script.js
@@ -0,0 +1,9 @@
+// EJERCICIO 5
+
+// creando variables
+let texto;
+let largoTexto;
+
+// solicitar al usuario
+texto = prompt("Ingrese un texto");
+alert("El largo del texto ingresado es: "+texto.length);
