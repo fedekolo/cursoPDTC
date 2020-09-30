5c96ae871e8dc3c05278d4163bde2c1e4c3ebaef
Fede Kolo <fedekolonski@gmail.com>
2020-09-30 20:20:51 -0300

    ej13

---
+++ Programacion web/Clase 13/js/script.js
@@ -0,0 +1,63 @@
+// EJERCICIO 1
+
+// let ensaladaDeFrutas = ["manzana" , "banana", "naranja" , "frutilla"];
+
+// for (let i = 0; i < ensaladaDeFrutas.length; i++) {
+//     if (i===0) {
+//         console.log("Las frutas que hay en la ensalada son:");
+//     }
+//     console.log(ensaladaDeFrutas[i]);
+// }
+
+// EJERCICIO 2 (SIN TERMINAR)
+
+// let toDoList = ["limpiar","cocinar","ordenar","trabajar","hacer ejercicio"];
+
+// EJERCICIO 3
+
+// let edades = [10,15,28,39,48,51,75,98];
+// let edadTotal = 0;
+
+// for (let i = 0; i < edades.length; i++) {
+//     edadTotal = edadTotal+edades[i];
+// }
+
+// console.log(edadTotal/edades.length);
+
+// EJERCICIO 4
+
+// let listaDeSuper = [];
+
+// for (let i = 0; i < 5; i++) {
+    
+//     if (listaDeSuper.length===0) {
+//         listaDeSuper.push(prompt("Agregue un producto"));
+//     } else {
+//         listaDeSuper.push(prompt("Agregue otro producto"));
+//     }
+    
+// }
+
+// for (let o = 0; o < listaDeSuper.length; o++) {
+//     console.log(listaDeSuper[o]);
+// }
+
+// EJERCICIO 5
+
+// let nombres = ["franco","federico","lu","juli","benja","nico"];
+// let letrasNombres = 0;
+// let nombreLargo;
+
+// for (let i = 0; i < nombres.length; i++) {
+//     if (letrasNombres < nombres[i].length) {
+//         nombreLargo = nombres[i];
+//         letrasNombres = nombres[i].length;
+//     } 
+    
+// }
+
+// console.log(nombreLargo);
+
+
+
+
