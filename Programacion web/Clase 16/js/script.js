b5c737d7b5f72bc18992acec132124fe472a7281
fedekolo <fedekolonski@gmail.com>
2020-10-11 16:36:08 -0300

    inicio clase 16

---
+++ Programacion web/Clase 16/js/script.js
@@ -0,0 +1,10 @@
+// EJERCICIO 1
+
+let toDoList = {
+    tipo_de_tarea = "Compras",
+    dia = "21/03",
+    productos = ["pure","fideos","arroz","agua","esponja","coca","bananas"],
+    cantidad_de_productos = "1",
+    estado = "pendientes",
+    
+}
